fb9c7190c1a3729faf202ae38a767b682840e2d2
Michelle Jiang <mhjiang@smith.edu>
2025-09-21 20:23:33 -0400

    okay i got the chatbot to respond when prompted and it responds for the correct number of rounds (still need to be able to input the rounds though..) i'm trying to add automatic randomized responses now using arraylist

--- Conversation.java
+++ Conversation.java
@@ -1,20 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner; // imports scanner class
 
 class Conversation implements ConversationRequirements {
 
   // Attributes 
-
-  /**
+  int rounds; // counts number of rounds, taken as input
+  //String transcript; // records transcript of conversation 
+  ArrayList<String> responses = new ArrayList<String>();
+  /** 
    * Constructor 
    */
-  Conversation() {
-    
+  public Conversation(int rounds, ArrayList<String> responses) {
+    this.rounds = rounds;
+    this.responses = [""];
+    //this.transcript = transcript; 
   }
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
-
+    System.out.println("Welcome! What's up?");
+    String yap = ""; // initializes user input
+    Scanner input = new Scanner(System.in); 
+    while (rounds > 0) {
+      // take in input
+      yap = input.nextLine();
+      System.out.println(respond(yap)); // read input & print output
+      rounds--; // repeat until rounds = 0
+    }
+    System.out.println("Bye!");
   }
 
   /**
@@ -30,15 +45,16 @@ class Conversation implements ConversationRequirements {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = ""; 
+    String returnString = "Hmm..."; 
     return returnString; 
   }
 
   public static void main(String[] arguments) {
 
-    Conversation myConversation = new Conversation();
+    Conversation myConversation = new Conversation(3);
     myConversation.chat();
     myConversation.printTranscript();
+    myConversation.respond(""); 
 
   }
 }
