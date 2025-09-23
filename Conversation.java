2407d5ca7115a838af91f09e26d9d4b3c1fba764
Michelle Jiang <mhjiang@smith.edu>
2025-09-23 10:32:25 -0400

    fixed errors, got the random list selection working, added comments for mirroring

--- Conversation.java
+++ Conversation.java
@@ -1,18 +1,23 @@
+/**
+ * File name: Conversation.java
+ * Author: Michelle Jiang 
+ * Description: 
+ */
 import java.util.Scanner; // imports scanner class
+import java.lang.Math; // inport Random class
 
 class Conversation implements ConversationRequirements {
 
   // Attributes 
   int rounds; // counts number of rounds, taken as input
-  //String transcript; // records transcript of conversation 
+  String[] transcript; // records transcript of conversation in a list then add everything into the list
   String[] responses; 
   /** 
    * Constructor 
    */
   public Conversation() {
-    this.rounds = rounds;
-    this.responses = new String[] {"Hmm...", "Uh huh...", "Tell me more...", "Mmm-hm.", "Really?", "How interesting!", "That's cool!", "Wow!", "Nice."};
-    //this.transcript = transcript; 
+    this.responses = new String[] {"Hmm...", "Uh huh...", "Tell me more...", "Mmm-hm.", "Really?", "How interesting!", "That makes sense.", "I hear you.", "I see.", "How intriguing."};
+    this.transcript = new String[] {}; 
   }
 
   /**
@@ -21,15 +26,17 @@ class Conversation implements ConversationRequirements {
   public void chat() {
     Scanner input = new Scanner(System.in); 
     System.out.println("How many rounds?");
-    rounds = input.nextInt();
+    String inputRounds = input.nextLine();
+    rounds = Integer.parseInt(inputRounds); 
     System.out.println("Hi there!  What's on your mind?");
     String yap; // initializes user input
     while (rounds > 0) {
       // take in input
-      yap = input.nextLine(); // issue: prints out response before taking in input...
+      yap = input.nextLine(); // issue: prints out response before taking in input
       System.out.println(respond(yap)); // read input & print output
       rounds--; // repeat until rounds = 0
     }
+    input.close();
     System.out.println("See ya!");
   }
 
@@ -37,7 +44,7 @@ class Conversation implements ConversationRequirements {
    * Prints transcript of conversation
    */
   public void printTranscript() {
-
+// create an array as an attribute and add every input and output into the array then print out the entire array here with new lines  
   }
 
   /**
@@ -46,7 +53,15 @@ class Conversation implements ConversationRequirements {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    String returnString = "Hmm..."; 
+    /**
+     * takes input string convert to array using spaces as the separators then search through each index for the correct word using regex? and replace then output
+     * iterate throguh string word by word use mirror dictionary to replace the words  
+     * prof recommends looking at the string java doc and using the list of methods (replaceAll using regex) on the inputstring
+     * perhaps trim white space? need iterations 
+     * take inputstring mark mirror words in a unique way then replace the mirrored words
+     */
+    int random = (int)(Math.random() * responses.length);
+    String returnString = responses[random]; 
     return returnString; 
   }
 
--- reflection.md
+++ reflection.md
@@ -3,6 +3,9 @@ Use this file to record your reflection on this assignment.
 - Which classmates did you consult with while working on this assignment?
 Yuhan, Lois 
 - Which session(s) of TA / office hours did you attend?
+
 - What are your initial impressions of Java? 
+Surprised there is no direct list function/type in Java 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
