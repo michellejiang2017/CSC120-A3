acf9ca0701a59ae3f36724b0c3fbeb3247fe7360
Michelle Jiang <mhjiang@smith.edu>
2025-09-23 21:56:16 -0400

    Finished basic requirements but may need to debug

--- Conversation.java
+++ Conversation.java
@@ -1,23 +1,46 @@
 /**
  * File name: Conversation.java
- * Author: Michelle Jiang 
- * Description: 
+ * Author Michelle Jiang 
+ * Description: Simple chatbot who will mirror inputted messages or give canned responses. Also prints transcript. 
+ * Date: 2025-09-23
  */
-import java.util.Scanner; // imports scanner class
-import java.lang.Math; // inport Random class
 
+// Imports for lists, maps, user input, and random number generator used in chatbot
+import java.util.ArrayList; 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner; 
+import java.lang.Math; 
+
+/**
+ * Conversation class which implements ConversationRequirements interface. 
+ * 
+ * Creates a simple chatbot which mirrors responses and gives canned responses. Also prints transcript. 
+ */
 class Conversation implements ConversationRequirements {
 
   // Attributes 
   int rounds; // counts number of rounds, taken as input
-  String[] transcript; // records transcript of conversation in a list then add everything into the list
+  ArrayList<String> transcript; 
   String[] responses; 
+  Map<String, String> mirrorWords;
+
   /** 
    * Constructor 
    */
   public Conversation() {
     this.responses = new String[] {"Hmm...", "Uh huh...", "Tell me more...", "Mmm-hm.", "Really?", "How interesting!", "That makes sense.", "I hear you.", "I see.", "How intriguing."};
-    this.transcript = new String[] {}; 
+    this.transcript = new ArrayList<String> (); 
+    this.mirrorWords = new HashMap<String, String>();
+    // Inserts the required mirrored words
+    mirrorWords.put("I", "you");
+    mirrorWords.put("me", "you");
+    mirrorWords.put("am", "are");
+    mirrorWords.put("you", "I");
+    mirrorWords.put("my", "your");
+    mirrorWords.put("your", "my");
+    mirrorWords.put("are", "am");
   }
 
   /**
@@ -26,26 +49,37 @@ class Conversation implements ConversationRequirements {
   public void chat() {
     Scanner input = new Scanner(System.in); 
     System.out.println("How many rounds?");
-    String inputRounds = input.nextLine();
-    rounds = Integer.parseInt(inputRounds); 
+    transcript.add("How many rounds?");
+
+    String inputRounds = input.nextLine(); 
+    transcript.add(inputRounds);
+    rounds = Integer.parseInt(inputRounds); // fixed for issue where it takes new line as userInput 
+    
     System.out.println("Hi there!  What's on your mind?");
-    String yap; // initializes user input
+    transcript.add("Hi there!  What's on your mind?");
+    String userInput; // initializes user input
+
     while (rounds > 0) {
       // take in input
-      yap = input.nextLine(); // issue: prints out response before taking in input
-      System.out.println(respond(yap)); // read input & print output
+      userInput = input.nextLine(); 
+      transcript.add(userInput);
+      System.out.println(respond(userInput)); // read input & print output
       rounds--; // repeat until rounds = 0
     }
+
     input.close();
     System.out.println("See ya!");
+    transcript.add("See ya!");
   }
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
-// create an array as an attribute and add every input and output into the array then print out the entire array here with new lines  
-  }
+    String output = String.join("\n", transcript);
+    System.out.println("\nTRANSCRIPT: ");
+    System.out.println(output);
+}
 
   /**
    * Gives appropriate response (mirrored or canned) to user input
@@ -53,15 +87,29 @@ class Conversation implements ConversationRequirements {
    * @return mirrored or canned response to user input  
    */
   public String respond(String inputString) {
-    /**
-     * takes input string convert to array using spaces as the separators then search through each index for the correct word using regex? and replace then output
-     * iterate throguh string word by word use mirror dictionary to replace the words  
-     * prof recommends looking at the string java doc and using the list of methods (replaceAll using regex) on the inputstring
-     * perhaps trim white space? need iterations 
-     * take inputstring mark mirror words in a unique way then replace the mirrored words
-     */
+    // initialize variables 
     int random = (int)(Math.random() * responses.length);
-    String returnString = responses[random]; 
+    boolean replaced = false;
+    String returnString = "";
+
+    // converts inputString to ArrayList by splitting through String[]
+    String[] strSplit = inputString.split(" ");
+    ArrayList<String> inputArray = new ArrayList<String>(Arrays.asList(strSplit));
+    for (int i=0;i < inputArray.size();i++) {
+      if (mirrorWords.containsKey(inputArray.get(i))) {
+        inputArray.set(i, mirrorWords.get(inputArray.get(i)));
+        replaced = true;
+      }
+    }
+
+    if (replaced) {
+      // convert inputArray back to a String by joining through String[]
+      String[] strArray = inputArray.toArray(new String[0]);
+      returnString = String.join(" ", strArray) + "?";
+    } else {
+      returnString = responses[random]; 
+    }
+    transcript.add(returnString);
     return returnString; 
   }
 
@@ -70,7 +118,5 @@ class Conversation implements ConversationRequirements {
     Conversation myConversation = new Conversation();
     myConversation.chat();
     myConversation.printTranscript();
-    myConversation.respond(""); 
-
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,11 +1,15 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
-Yuhan, Lois 
+    Yuhan, Lois 
 - Which session(s) of TA / office hours did you attend?
-
+    Tues Sept 23 2025 TA hours with Sofia 
 - What are your initial impressions of Java? 
-Surprised there is no direct list function/type in Java 
+    Surprised there is no direct list function/type in Java, the need to specify types is pretty annoying to be honest but I understand why it can be helpful. I also did not enjoy needing to convert from String to String[] to ArrayList<String> and back, perhaps there's a different method that could be used but with my Java knowledge and asking the TAs this is was the best choice. 
+    What I did like was that the usage of methods was pretty easy to implement and test. 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
-
+    I don't have any great conclusions. Java is much less intuitive than Python for me and I find it quite difficult to think about in terms of classes and types and methods. But I'm sure it's great for the computer! 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    Going to TA hours was fun because not only were there wonderful TAs who could help but there were also other students who were struggling with me. I found that quite helpful, both in terms of completing the assignment and in terms of morale. 
+    What didn't work was using the String methods and trying to use replaceAll() because I felt like it was too finicky and difficult to work with in terms of needing to search for bugs and hard code the bugs out and I felt it was quite repetitive. However, maybe a smarter person than I could've done it I was simply already using the Map and felt that was the most intuitive method for me. 
+    Some advice I would give is ask for help! I hate asking for help and I did it! It was really helpful! Another thing is to use the Java documentation for everything, it's very extensive and has some great resources, same with w3schools and basically just Googling things was very helpful. 
